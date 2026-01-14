49bb3ed96749c268fad02dff9679212e7c3ce3f1
Hammad <hammadpat@gmail.com>
2026-01-13 20:15:00 -0500

    initial start to slow refactor

--- preload.js
+++ preload.js
@@ -1,3 +1,10 @@
+// This preload script sets up secure communication between the renderer process
+// and the main process using Electron's contextBridge and ipcRenderer.
+// Preload script contains code that runs before my web page is loaded into the browser window.
+// Often used to expose APIs to the renderer process in a secure way.
+
+// Listners are supposed to be defined in main.js, this file just exposes the ipcRenderer methods.
+
 const { contextBridge, ipcRenderer } = require('electron');
 
 // Track if listeners are already registered to prevent duplicates
@@ -6,46 +13,40 @@ let gamepadListener = null;
 let analogListener = null;
 let scrollListener = null;
 
+// This exposes a secure API to the renderer process
+
 contextBridge.exposeInMainWorld('electronAPI', {
+  /*
   onClickThroughChanged: (callback) => {
-    // Remove existing listener if any
-    if (clickThroughListener) {
-      ipcRenderer.removeListener('click-through-changed', clickThroughListener);
-    }
     clickThroughListener = (event, enabled) => callback(enabled);
     ipcRenderer.on('click-through-changed', clickThroughListener);
-  },
+  },*/
+
+  // Gamepad input listener
   onGamepadAction: (callback) => {
-    if (gamepadListener) {
-      ipcRenderer.removeListener('gamepad-action', gamepadListener);
-    }
     gamepadListener = (event, action) => callback(action);
     ipcRenderer.on('gamepad-action', gamepadListener);
   },
+
+  // Analog input listener
   onAnalogInput: (callback) => {
-    if (analogListener) {
-      ipcRenderer.removeListener('analog-input', analogListener);
-    }
     analogListener = (event, data) => callback(data);
     ipcRenderer.on('analog-input', analogListener);
   },
+
+  // Scroll input listener
   onGamepadScroll: (callback) => {
-    if (scrollListener) {
-      ipcRenderer.removeListener('gamepad-scroll', scrollListener);
-    }
     scrollListener = (event, data) => callback(data);
     ipcRenderer.on('gamepad-scroll', scrollListener);
   },
-  closeWindow: () => {
-    ipcRenderer.send('close-window');
-  },
-  startResize: (direction) => {
-    ipcRenderer.send('start-resize', direction);
-  },
-  stopResize: () => {
-    ipcRenderer.send('stop-resize');
-  },
-  setOpacity: (opacity) => {
-    ipcRenderer.send('set-opacity', opacity);
-  },
+
+  // Close window
+  closeWindow: () => ipcRenderer.send('close-window'),
+  // Resize window
+  startResize: (direction) => ipcRenderer.send('start-resize', direction),
+
+  stopResize: () => ipcRenderer.send('stop-resize'),
+
+  // Set window opacity
+  setOpacity: (opacity) => ipcRenderer.send('set-opacity', opacity),
 });
